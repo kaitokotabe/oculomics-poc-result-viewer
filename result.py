8e05d5cdab4087dc8398cffb63953ca2d8ace5cb
kaitokotabe <kaito.kotabe.d2@tohoku.ac.jp>
2025-09-08 17:00:08 +0900

    Update result.py

--- result.py
+++ result.py
@@ -17,6 +17,7 @@ from reportlab.pdfbase.ttfonts import TTFont
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_ANON_KEY = st.secrets["SUPABASE_ANON_KEY"]  # RLS用
 supabase = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
+st.write(supabase_URL)
 
 # フォント登録
 FONT_PATH = os.path.join(os.path.dirname(__file__), "fonts", "ipaexg.ttf")
